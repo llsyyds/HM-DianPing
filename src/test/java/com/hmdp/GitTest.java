8957b41af3aedd469b80fa35028f861392d014c7
lls <2892194380@qq.com>
2023-05-13 10:07:29 +0800

    second commit

---
+++ src/test/java/com/hmdp/GitTest.java
@@ -0,0 +1,14 @@
+package com.hmdp;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author lls
+ * @date 2023-2023/5/13-10:04
+ */
+public class GitTest {
+    @Test
+    void pushTest(){
+
+    }
+}
